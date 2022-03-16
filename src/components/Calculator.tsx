8a4208056219ea8279a63ea7e84b29cf6ad87e0b
gmccullers <mccullers.ghameerah@gmail.com>
2022-03-16 12:46:15 -0400

    set up typescript/styled components

---
+++ src/components/Calculator.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background: #323232;
+`;
+
+const Calculator: React.FC<{}> = () => {
+  return <div></div>;
+};
+
+export default Calculator;
